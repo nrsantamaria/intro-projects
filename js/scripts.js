f3e575d83e792cf64f0725fbcd3dd720307b43e2
jin camou and Nicole Santamaria <jin@>
2017-03-22 15:25:48 -0700

    debugged scripts.js by removind id tag from var blanks to indicate each related span in story

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var cat1Input = $("input#cat1").val();
-    var cat2Input = $("input#cat2").val();
-    var fruitInput= $("input#fruit").val();
-    var adjectiveInput = $("input#adjective").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-    // var spanOutput = $("output#story").val();
+  $("#blanks form").submit(function(event) {
+    var blanks = ["cat1", "cat2", "fruit", "adjective", "verb", "noun"];
+    // var cat1Input = $("input#cat1").val();
+    // var cat2Input = $("input#cat2").val();
+    // var fruitInput= $("input#fruit").val();
+    // var adjectiveInput = $("input#adjective").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
 
-    $(".cat1").text(cat1Input.toUpperCase());
-    $(".cat2").text(cat2Input.toUpperCase());
-    $(".fruit").text(fruitInput.toUpperCase());
-    $(".adjective").text(adjectiveInput.toUpperCase());
-    $(".verb").text(verbInput.toUpperCase());
-    $(".noun").text(nounInput.toUpperCase());
-
+    // $(".cat1").text(cat1Input.toUpperCase());
+    // $(".cat2").text(cat2Input.toUpperCase());
+    // $(".fruit").text(fruitInput.toUpperCase());
+    // $(".adjective").text(adjectiveInput.toUpperCase());
+    // $(".verb").text(verbInput.toUpperCase());
+    // $(".noun").text(nounInput.toUpperCase());
+    //
+    // $("#story").show();
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      // console.log(userInput);
+      $("." + blank).text(userInput).val();
+      // console.log(userInput);
+    });
     $("#story").show();
-
     event.preventDefault();
   });
 });
