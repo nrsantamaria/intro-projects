62536eca215e75e3d5685328c7bb9a6ca9231032
jin camou and Nicole Santamaria <nicole@>
2017-03-22 15:16:41 -0700

    debugged scripts.js by removind id tag from var blanks to indicate each related span in order

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,16 @@
 // business backend logic
-var blanks = ["#requester", "#recipient", "#song", "#phone-number", "#date", "#time"];
-
-
-
-
 
 
 
 // user interface / frontend logic
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+    var blanks = ["requester", "recipient", "song", "phone-number", "date", "time"];
     blanks.forEach(function(blank){
-      var userInput = $("#input" + blank).val();
+      var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput).val();
-
     });
-    $("#order").show();
-    event.preventDefault();
+  $("#order").show();
+  event.preventDefault();
   });
-
-
 });
