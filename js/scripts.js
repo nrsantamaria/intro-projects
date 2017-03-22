4db4eccd0ccb829277d00d5fca9762a6fb3edc5c
jin camou and Nicole Santamaria <nicole@>
2017-03-22 09:57:52 -0700

    update scripts.js to add toUpperCase to variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+  $("#formOne").submit(function(event) {
     var cat1Input = $("input#cat1").val();
     var cat2Input = $("input#cat2").val();
     var fruitInput= $("input#fruit").val();
     var adjectiveInput = $("input#adjective").val();
     var verbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
+    // var spanOutput = $("output#story").val();
 
-
-    $(".cat1").text(cat1Input);
-    $(".cat2").text(cat2Input);
-    $(".fruit").text(fruitInput);
-    $(".adjective").text(adjectiveInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".cat1").text(cat1Input.toUpperCase());
+    $(".cat2").text(cat2Input.toUpperCase());
+    $(".fruit").text(fruitInput.toUpperCase());
+    $(".adjective").text(adjectiveInput.toUpperCase());
+    $(".verb").text(verbInput.toUpperCase());
+    $(".noun").text(nounInput.toUpperCase());
 
     $("#story").show();
 
