bf2bb25141e7fefaf6ba6bb31cef670054fd0559
James Higgins and Nicole Santamaria <nicole@>
2017-03-23 10:19:15 -0700

    add car insurance quote page that gave custom response based on text, select and radio button inputs.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form#insurance").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+    var color = $("input:radio[name=color]:checked").val();
+
+
+
+  var quote = (100 - age) * 3;
+  if (gender === "male" && age < 26 && color === "red") {
+    quote +=250;
+  }
+
+    $("#rate").empty().append(quote);
+    $("#quote").show();
+
+    event.preventDefault();
+  });
+});
