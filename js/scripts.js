9a9c04035a648ba45298d06cf056f7c51cb512f8
Dana Weiss and Nicole Santamaria <dana+nicole@>
2017-03-28 14:35:50 -0700

    add index.html, and css and js directories

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
-  var vowels = ["a", "e", "i", "o", "u"];
-  var puzzle = "supercalifragilisticexpialidocious";
-  var thePuzzle = puzzle.substring(0);
-    for (var index = 0; index < vowels.length; index ++) {
-			var thePuzzle = thePuzzle.split(vowels[index]).join("-");
-
-    }
-    $(".puzzle").text(puzzle);
-    $(".thePuzzle").text(thePuzzle);
+  $("form#transportation_survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val();
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
+    $('#transportation_survey').hide();
+  });
 });
