ce45a34c79530899166652aa026250ee9c3d7dd8
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-03-29 06:56:58 -0700

    if year is divisible by four output should detail true.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
-      return false;
+      if (year % 4 === 0) {
+        return true;
+      } else {
+        return false;
+      }
     };
+
 // jQuery user interface logic here.
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
